7ab6e380b8b89f59244b2d6fd098fd47f2e6cf44
support <support@DESKTOP-2BH1VSI>
2021-11-12 20:10:42 +0100

    Add project files.

---
+++ Exercise/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    public class PlaneShape
+    {
+        double CalcPerimeter(List<IPlaneShape> AllShapes)
+        {
+            double perimeter = 0;
+            foreach (var item in AllShapes)
+            {
+                perimeter += item.Perimeter();
+            }
+            return perimeter;
+        }
+    }
+
+    public class Rectangle: IPlaneShape
+    {
+        public double Height { get; set; }
+        public double Width { get; set; }
+
+        public double Perimeter()
+        {
+            return Height * Width;
+        }
+    }
+
+    public class Square : IPlaneShape
+    {
+        public double Width { get; set; }
+
+        public double Perimeter()
+        {
+            return Math.Pow(Width, 2);
+        }
+    }
+
+    interface IPlaneShape
+    {
+        public double Perimeter();
+    }
+
+
+}
